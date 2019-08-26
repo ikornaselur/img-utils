ce51ec88b9aad191a690e7d0226d15dd0609a1f3
Axel Örn Sigurðsson <axel@absalon.is>
2019-08-26 18:47:15 +0200

    Update bench script and add test files

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import time
 import img_utils
 from PIL import Image
 
-EXAMPLE_FILE = "./example.jpg"
-
-ARGS = (EXAMPLE_FILE, 80, 220)
-
-
-def rust_impl(src_path, dst_path, amount=75, cutoff=220):
-    img_utils.increase_contrast(src_path, dst_path, amount, cutoff)
+FILES = {"small": "test-small.jpg", "normal": "test-normal.jpg"}
+AMOUNT = 75
+CUTOFF = 127
 
 
-def python_impl(file_name, amount=75, cutoff=220):
-    original = Image.open(file_name)
+def python_impl(in_file: str, out_file: str, amount: int, cutoff: int):
+    original = Image.open(in_file)
     original_pixels = original.load()
 
     new_image = Image.new(original.mode, original.size)
@@ -29,20 +25,17 @@ def python_impl(file_name, amount=75, cutoff=220):
                 b - int(b * amount / 100) if lower else b,
             )
 
-    with open("out-python.jpg", "wb") as f:
+    with open(out_file, "wb") as f:
         new_image.save(f, format=original.format)
 
 
 if __name__ == "__main__":
 
-    def python():
-        python_impl(EXAMPLE_FILE)
-
-    def bench(func, max_runtime=5):
+    def bench(func, args, max_runtime=5):
         start = time.time()
         runs = 0
         while time.time() - start < max_runtime:
-            func()
+            func(*args)
             runs += 1
         total = time.time() - start
         avg = total / runs * 1000  # ms
@@ -51,16 +44,24 @@ if __name__ == "__main__":
 
         return avg
 
-    def rust():
-        rust_impl(EXAMPLE_FILE, "out-rust.jpg")
+    def rust(in_file: str, out_file: str):
+        img_utils.increase_contrast(in_file, out_file, AMOUNT, CUTOFF)
 
-    print()
-    py_avg = bench(python)
-    ru_avg = bench(rust)
-    print()
+    def python(in_file: str, out_file: str):
+        python_impl(in_file, out_file, AMOUNT, CUTOFF)
 
-    diff = py_avg / ru_avg
-    if diff > 1:
-        print(f"Rust is {diff:.02f}x faster")
-    else:
-        print(f"Rust is {1/diff:.02f}x slower")
+    for size, file in FILES.items():
+        in_path = f"test-img/{file}"
+        out_path = f"/tmp/{file}"
+
+        print(f"*** Testing {size} size")
+        py_avg = bench(python, args=[in_path, out_path])
+        ru_avg = bench(rust, args=[in_path, out_path])
+        print()
+
+        diff = py_avg / ru_avg
+        if diff > 1:
+            print(f"Rust is {diff:.02f}x faster for {size} size")
+        else:
+            print(f"Rust is {1/diff:.02f}x slower for {size} size")
+        print()
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use img_utils::increase_contrast;
 
 
 fn main() {
-    increase_contrast(String::from("./example.jpg"), String::from("out.jpg"), 80, 220).unwrap();
+    increase_contrast(String::from("test.jpg"), String::from("test-out.jpg"), 80, 220).unwrap();
 }
