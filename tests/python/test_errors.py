3091b15fe1337324d60d05831057f42220fb62bd
Axel Örn Sigurðsson <axel@absalon.is>
2019-09-09 19:51:31 +0100

    Use lib exceptions in python error tests

--- tests/python/test_errors.py
+++ tests/python/test_errors.py
@@ -3,17 +3,16 @@ import tempfile
 import mock
 import pytest  # type: ignore
 from img_utils import darken_pixels
-from img_utils.exceptions import (FileNotFoundException,
-                                  UnsupportedFormatException)
+from img_utils.img_utils import exceptions
 
 
 def test_darken_pixels_file_not_found():
-    with pytest.raises(FileNotFoundException):
+    with pytest.raises(exceptions.FileNotFoundException):
         darken_pixels("/non/existant/file.jpg", "/out.jpg", 0, 0)
 
 
 def test_darken_pixels_invalid_file_type():
-    with pytest.raises(UnsupportedFormatException):
+    with pytest.raises(exceptions.UnsupportedFormatException):
         darken_pixels("./tests/assets/bin", "/out.jpg", 0, 0)
 
 
